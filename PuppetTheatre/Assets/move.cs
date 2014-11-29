239c0b73f40b8cf1e2947aeb831580f6feb594c6
Brecon White <breconwhite@hotmail.com>
2014-11-29 00:44:47 -0500

    Beta Update
    Engine, models, etc uploaded

---
+++ PuppetTheatre/Assets/move.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public Rigidbody HandR;
+	public Rigidbody HandL;
+	public Rigidbody FootR;
+	public Rigidbody FootL;
+
+	Vector3 footRDefault;
+	Vector3 footLDefault;
+
+	public puppeteer RhandControl;
+	public puppeteer LhandControl;
+
+	bool isStep;
+	float stepTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+		footLDefault = FootL.transform.position;
+		footRDefault = FootR.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		this.rigidbody.AddRelativeForce(transform.forward, ForceMode.Impulse);
+
+		//HandL.transform.position = new Vector3(LhandControl.getPointer.x/5 + this.transform.position.x,LhandControl.getPointer.y + this.transform.position.y, 15);
+		//HandR.transform.position = new Vector3(RhandControl.getPointer.x/5 + this.transform.position.x,RhandControl.getPointer.y + this.transform.position.y, 15);
+
+		if (isStep) {
+			stepTimer += 1 * Time.deltaTime;
+		}
+
+		if(stepTimer > 0.5f)
+		{
+			stepTimer = 0;
+			isStep = false;
+		}
+
+		if(LhandControl.thumbUp)
+		{
+			FootL.AddForce(new Vector3(.5f,.5f,0) ,ForceMode.Impulse);
+
+			this.rigidbody.transform.Rotate(Vector3.up);
+		}
+
+		if(RhandControl.thumbUp)
+		{
+			FootR.AddForce(new Vector3(.5f,.5f,0) ,ForceMode.Impulse);
+
+			this.rigidbody.transform.Rotate(Vector3.down);
+		}
+	}
+}
---
+++ PuppetTheatre/Assets/test.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	
+	public Rigidbody HandR;
+	public Rigidbody HandL;
+	public Rigidbody FootR;
+	public Rigidbody FootL;
+	
+	public puppeteer puppetL;
+	public puppeteer puppetR;
+
+	Vector3 footRDefault;
+	Vector3 footLDefault;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		footLDefault = FootL.transform.position;
+		footRDefault = FootR.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 xyLVector = puppetL.index.transform.localPosition;
+		Vector3 xyRVector = puppetR.index.transform.localPosition;
+
+		HandL.transform.localPosition = new Vector3 (xyLVector.x * 2 - 10, xyLVector.y, 4);
+
+		HandR.transform.localPosition = new Vector3 (xyRVector.x * 2 + 5, xyRVector.y, 4);
+
+		if(puppetL.thumbUp)
+	   	{
+			this.transform.Rotate(Vector3.up);
+		}
+
+		if(puppetR.thumbUp)
+		{
+			this.transform.Rotate(Vector3.down);
+			FootR.AddForce(Vector3.up);
+		}
+
+		if(puppetL.thumbUp || puppetR.thumbUp)
+		{
+			//this.transform.Translate(Vector3.forward/20, this.transform);
+			this.rigidbody.AddRelativeForce(Vector3.forward, ForceMode.Impulse);
+		}
+	}
+}
